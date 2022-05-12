7061feeb89217eebf8e3b5e1e930a08ced71901f
aryanc3927 <96081672+aryanc3927@users.noreply.github.com>
2022-05-12 16:22:27 -0700

    scraper 0.5

--- scraper.py
+++ scraper.py
@@ -4,7 +4,6 @@ import csv
 from random import randint
 from time import sleep
 
-dictionary = {}
 # input url and output num views of video
 def scrape(url):
   soup = BeautifulSoup(requests.get(url).text, "html.parser")
@@ -25,15 +24,20 @@ def main():
     id_list = id_list[1:]
 
     # map ids to respective view counts
+    dictionary = {}
     for i in range(len(id_list)):
         url = "https://www.youtube.com/watch?v=" + id_list[i]
         views = scrape(url)
-        sleep(1)
+        sleep(randint(1, 5))
         if views == 0:
             break
         dictionary[id_list[i]] = views
         print(i, views)
-
+    
+    with open('viewcounts.csv', 'w', newline='') as file:
+        writer = csv.writer(file)
+        for key in dictionary: 
+            writer.writerow([key, dictionary[key]])
 
 if __name__ == "__main__":
     main()
