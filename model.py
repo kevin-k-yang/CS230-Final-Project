da225ca9e7ad2b666792cc8a6d90d3a5eab423b6
Kevin Yang <kevvyang@stanford.edu>
2022-05-16 22:56:13 -0700

    Created a way to parse viewcounts

--- model.py
+++ model.py
@@ -6,40 +6,35 @@
 #
 
 # imports
-# import numpy as np
-# import tensorflow as tf
-# from tensorflow import keras
-# from keras import layers
-# from keras.layers import Input, Dense, Activation, ZeroPadding2D, BatchNormalization, Flatten, Conv2D
-# from keras.layers import AveragePooling2D, MaxPooling2D, Dropout, GlobalMaxPooling2D, GlobalAveragePooling2D
-# from keras.models import Model
-# from keras.preprocessing import image
-# from keras.utils import layer_utils
-# from keras.utils.data_utils import get_file
-# from keras.applications.imagenet_utils import preprocess_input
-# import pydot
-# from IPython.display import SVG
-# from keras.utils.vis_utils import model_to_dot
-# from keras.utils import plot_model
-# from kt_utils import *
-
-# import keras.backend as K
-# K.set_image_data_format('channels_last')
-# import matplotlib.pyplot as plt
-# from matplotlib.pyplot import imshow
-
 import matplotlib.pyplot as plt
 import numpy as np
 import PIL
 import tensorflow as tf
+import csv
 from tensorflow.keras.layers import Dense, Flatten
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.optimizers import Adam
 
 
+def parse_view_counts(link):
+    view_count_map = {}
+    with open(link) as csv_file:
+        csv_reader = csv.reader(csv_file, delimiter=',')
+        first_line = True
+        for row in csv_reader:
+            if not first_line:
+                view_count_map[row[0]] = row[1]
+            else:
+                first_line = False
+
+    return view_count_map
+
+
 # Implement ResNet 50 pretrained weights for this task
 # This approach is borrowed from https://chroniclesofai.com/transfer-learning-with-keras-resnet-50/
 def main():
+    # parse the views
+    view_count_map = parse_view_counts("viewcounts.csv")
     # preprocess images
     data_dir = "images"
     img_height,img_width=180,180
@@ -50,16 +45,14 @@ def main():
         validation_split=0.2,
         subset="training",
         seed=123,
-        image_size=(img_height, img_width),
-        batch_size=batch_size)
+        image_size=(img_height, img_width), batch_size=batch_size)
     # validation set
     val_ds = tf.keras.preprocessing.image_dataset_from_directory(
         data_dir,
         validation_split=0.2,
         subset="validation",
         seed=123,
-        image_size=(img_height, img_width),
-        batch_size=batch_size)
+        image_size=(img_height, img_width), batch_size=batch_size)
 
     # load resnet model
     resnet_model = Sequential()
