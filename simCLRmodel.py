7a7e993a8f3b33615013a5a614a73bb91cc05c6d
aryanc3927 <96081672+aryanc3927@users.noreply.github.com>
2022-05-30 23:24:40 -0700

    got that bih

--- simCLRmodel.py
+++ simCLRmodel.py
@@ -8,7 +8,6 @@
 # imports
 import matplotlib.pyplot as plt
 import numpy as np
-import PIL
 import tensorflow as tf
 import tensorflow_hub as hub
 import tensorflow_datasets as tfds
@@ -43,27 +42,27 @@ def main():
     hub_path = 'gs://simclr-checkpoints/simclrv2/finetuned_100pct/r50_1x_sk0/hub/'
     model = hub.KerasLayer(hub_path, trainable=False)
     
-    nnclr_model = Sequential()
-    nnclr_model.add(model)
-    nnclr_model.add(Flatten())
-    nnclr_model.add(Dense(512, activation='relu'))
-    nnclr_model.add(Dense(1, activation='sigmoid'))
+    simclr_model = Sequential()
+    simclr_model.add(model)
+    simclr_model.add(Flatten())
+    simclr_model.add(Dense(512, activation='relu'))
+    simclr_model.add(Dense(1, activation='sigmoid'))
 
-    # # train model
-    # resnet_model.compile(optimizer=Adam(learning_rate=0.001),loss='binary_crossentropy',metrics=['accuracy'])
-    # history = resnet_model.fit(train_ds, validation_data=val_ds, epochs=10)
+    # train model
+    simclr_model.compile(optimizer=Adam(learning_rate=0.001),loss='binary_crossentropy',metrics=['accuracy'])
+    history = simclr_model.fit(train_ds, validation_data=val_ds, epochs=10)
 
-    # # display results
-    # fig1 = plt.gcf()
-    # plt.plot(history.history['accuracy'])
-    # plt.plot(history.history['val_accuracy'])
-    # plt.axis(ymin=0.4,ymax=1)
-    # plt.grid()
-    # plt.title('Model Accuracy')
-    # plt.ylabel('Accuracy')
-    # plt.xlabel('Epochs')
-    # plt.legend(['train', 'validation'])
-    # plt.show()
+    # display results
+    fig1 = plt.gcf()
+    plt.plot(history.history['accuracy'])
+    plt.plot(history.history['val_accuracy'])
+    plt.axis(ymin=0.4,ymax=1)
+    plt.grid()
+    plt.title('Model Accuracy')
+    plt.ylabel('Accuracy')
+    plt.xlabel('Epochs')
+    plt.legend(['train', 'validation'])
+    plt.show()
     
     return 0
 
